c39f9b661bc4ed6fbc0c802e63512c9e8d1c1a3b
Nikodim Nikodimov <nikodim.nikodimov@gmail.com>
2023-11-21 14:55:24 +0200

    add files

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Terrafrom Initialization
 terraform {
-#  required_version = ">= 0.13"
+  required_version = ">= 0.13"
   required_providers {
     vcd = {
       source = "vmware/vcd"
@@ -19,15 +19,3 @@ provider "vcd" {
   max_retry_timeout = var.vcd_max_retry_timeout
   allow_unverified_ssl = var.vcd_allow_unverified_ssl
 }
-
-#Create a new org names "T3"
-
-#resource "vcd_org" "org-name" {
-#  name = "T3"
-#  full_name = "My organization"
-#  description = "The pride of my work"
-#  is_enabled = "true"
-#  delete_recursive = "true"
-#  delete_force = "true"
-#}
-
